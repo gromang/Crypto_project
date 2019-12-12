d497e1c3d228656d749427feb60444fa7920b5d4
Serebrennikov <smit7bb@mail.ru>
2019-12-12 20:57:33 +0500

    converted data in get_data_huobi to the desired format

--- parser_huobi.py
+++ parser_huobi.py
@@ -18,15 +18,28 @@ def get_api_huobi(url, time_interval, number_of_data, trading_couple):
         return False
 
 
-def get_data_huobi(trading_couple='btcusdt', time_interval='1min', number_of_data=150):
+def get_data_huobi(trading_couple='btcusdt', time_interval='1min', number_of_data='150'):
     try:
         coin_data = get_api_huobi('https://api.huobi.pro/market/history/kline',
                                   time_interval, number_of_data, trading_couple)
-        # result_news = []
-        print(coin_data)
+
+        result_news = []
+        for coin_results in coin_data['data']:
+            result_news.append({
+                'Timestamp': coin_results['id'],
+                'Open': float(coin_results['open']),
+                'Close': float(coin_results['close']),
+                'High': float(coin_results['high']),
+                'Low': float(coin_results['low']),
+                'Volume': float(coin_results['amount']),
+
+            })
+
+        return result_news
     except TypeError:
+        print('Объект не найден')
         return False
 
 
-if __name__=="__main__":
-    print(get_data_huobi())
+if __name__ == "__main__":
+    print(len(get_data_huobi(trading_couple='ethusdt', number_of_data='300')))
