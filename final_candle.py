685b91764b7e26ef89875298990156392265d084
Moiseev Roman <lp15.mr@yandex.ru>
2019-12-13 19:27:41 +0300

    Add parsers

--- final_candle.py
+++ final_candle.py
@@ -1,18 +1,33 @@
+from parser_binance import *
 from parser_bitfinex import *
+from parser_hitbtc import *
+from parser_huobi import *
 from parser_kraken import *
+
 from collections import Counter
 
 
 def final_ohlcv(symbol, interval):
     ohlcv_list = []
     # Получаем последнюю свечу с каждой биржи
-    kraken_candle = get_data_bitfinex(symbol, interval)
     bitfinex_candle = get_data_kraken(symbol, interval)
+    binance_candle = get_data_binance(symbol, interval)
+    hitbtc_candle = get_data_hitbtc(symbol, interval)
+    huobi_candle = get_data_huobi(symbol, interval)
+    kraken_candle = get_data_bitfinex(symbol, interval)
+
     # Формируем список последних свечей
-    if kraken_candle:
-        ohlcv_list.append(kraken_candle)
     if bitfinex_candle:
         ohlcv_list.append(bitfinex_candle)
+    if binance_candle:
+        ohlcv_list.append(binance_candle)
+    if hitbtc_candle:
+        ohlcv_list.append(hitbtc_candle)
+    if huobi_candle:
+        ohlcv_list.append(huobi_candle)
+    if kraken_candle:
+        ohlcv_list.append(kraken_candle)
+
     sum_vol = 0
     sum_open = 0
     sum_close = 0
@@ -33,7 +48,7 @@ def final_ohlcv(symbol, interval):
         "Low": round(sum_low, 1),
         "Volume": sum_vol,
     }
-    logging.info(f"Finall {interval} candle for {symbol} equal : {sum_ohlcv}")
+    logging.info(f"Final {interval} candle for {symbol} equal : {sum_ohlcv}")
 
     return sum_ohlcv
 
