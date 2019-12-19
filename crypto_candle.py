fbd7d2256c8ac6ad44e67b25bf8a7a37a34f40a0
Roman Moiseev <lp15.mr@yandex.ru>
2019-12-19 10:33:56 +0300

    Add exceptions

--- crypto_candle.py
+++ crypto_candle.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class CryptoCandle:
     def __init__(self, symbol, interval="1m"):
-        logging.basicConfig(filename="parser.log", level=logging.INFO, filemode="w")
+        logging.basicConfig(filename="parser.log",level=logging.INFO, filemode="w")
         self.symbol = symbol.upper()
         self.interval = interval.lower()
         self.data = {}
@@ -26,24 +26,25 @@ class CryptoCandle:
         try:
             get_data = requests.get(f"{api_url}", params=params, timeout=5)
             get_data.raise_for_status()
-            logging.info(f"{exchange.upper()} API : {get_data.url}")
-            coin_data = get_data.json()
-            candle = coin_data[-2]
-            logging.info(f"{exchange.upper()} initial candle : {candle}")
-            # Преобразование данных к нужному формату
-            ohlcv = {
-                "Timestamp": round(candle[0] / 1000),
-                "Open": round(float(candle[1]), 1),
-                "Close": round(float(candle[4]), 1),
-                "High": round(float(candle[2]), 1),
-                "Low": round(float(candle[3]), 1),
-                "Volume": round(float(candle[5]), 10),
-            }
-            logging.info(f"{exchange.upper()} result : {ohlcv}")
-            return ohlcv
-        except (requests.RequestException, ValueError) as err:
-            logging.info(f"{exchange.upper()} ERROR: ", err)
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"{exchange.upper()} ERROR: {err}")
             return False
+        logging.info(f"{exchange.upper()} API : {get_data.url}")
+        coin_data = get_data.json()
+        candle = coin_data[-2]
+        logging.info(f"{exchange.upper()} initial candle : {candle}")
+        # Преобразование данных к нужному формату
+        ohlcv = {
+            "Timestamp": round(candle[0] / 1000),
+            "Open": round(float(candle[1]), 1),
+            "Close": round(float(candle[4]), 1),
+            "High": round(float(candle[2]), 1),
+            "Low": round(float(candle[3]), 1),
+            "Volume": round(float(candle[5]), 10),
+        }
+        logging.info(f"{exchange.upper()} result : {ohlcv}")
+        return ohlcv
+        
 
     def bitfinex(self):
         exchange = "bitfinex"
@@ -53,24 +54,25 @@ class CryptoCandle:
         try:
             get_data = requests.get(f"{api_url}{intr}:{sym}/hist?limit=2", timeout=5)
             get_data.raise_for_status()
-            logging.info(f"{exchange.upper()} API : {get_data.url}")
-            coin_data = get_data.json()
-            logging.info(coin_data)
-            candle = coin_data[-1]
-            logging.info(f"{exchange.upper()} initial candle : {candle}")
-            ohlcv = {
-                "Timestamp": int(candle[0] / 1000),
-                "Open": round(candle[1], 1),
-                "Close": round(candle[2], 1),
-                "High": round(candle[3], 1),
-                "Low": round(candle[4], 1),
-                "Volume": round(candle[5], 10),
-            }
-            logging.info(f"{exchange.upper()} result : {ohlcv}")
-            return ohlcv
-        except (requests.RequestException, ValueError) as err:
-            logging.info(f"{exchange.upper()} ERROR: ", err)
-            return False
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"{exchange.upper()} ERROR: {err}")
+            return False   
+        logging.info(f"{exchange.upper()} API : {get_data.url}")
+        coin_data = get_data.json()
+        logging.info(coin_data)
+        candle = coin_data[-1]
+        logging.info(f"{exchange.upper()} initial candle : {candle}")
+        ohlcv = {
+            "Timestamp": int(candle[0] / 1000),
+            "Open": round(candle[1], 1),
+            "Close": round(candle[2], 1),
+            "High": round(candle[3], 1),
+            "Low": round(candle[4], 1),
+            "Volume": round(candle[5], 10),
+        }
+        logging.info(f"{exchange.upper()} result : {ohlcv}")
+        return ohlcv
+        
 
     def hitbtc(self):
         exchange = "hitbtc"
@@ -84,25 +86,26 @@ class CryptoCandle:
         try:
             get_data = requests.get(f"{api_url}{sym}", params=params, timeout=5)
             get_data.raise_for_status()
-            logging.info(f"{exchange.upper()} API : {get_data.url}")
-            coin_data = get_data.json()
-            logging.info(coin_data)
-            candle = coin_data[-2]
-            logging.info(f"{exchange.upper()} initial candle : {candle}")
-            data_time = datetime.strptime(candle["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ")
-            ohlcv = {
-                "Timestamp": round(data_time.timestamp() + 10800),
-                "Open": round(float(candle["open"]), 1),
-                "Close": round(float(candle["close"]), 1),
-                "High": round(float(candle["max"]), 1),
-                "Low": round(float(candle["min"]), 1),
-                "Volume": round(float(candle["volume"]), 10),
-            }
-            logging.info(f"{exchange.upper()} result : {ohlcv}")
-            return ohlcv
-        except (requests.RequestException, ValueError) as err:
-            logging.info(f"{exchange.upper()} ERROR: ", err)
-            return False
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"{exchange.upper()} ERROR: {err}")
+            return False 
+        logging.info(f"{exchange.upper()} API : {get_data.url}")
+        coin_data = get_data.json()
+        logging.info(coin_data)
+        candle = coin_data[-2]
+        logging.info(f"{exchange.upper()} initial candle : {candle}")
+        data_time = datetime.strptime(candle["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        ohlcv = {
+            "Timestamp": round(data_time.timestamp() + 10800),
+            "Open": round(float(candle["open"]), 1),
+            "Close": round(float(candle["close"]), 1),
+            "High": round(float(candle["max"]), 1),
+            "Low": round(float(candle["min"]), 1),
+            "Volume": round(float(candle["volume"]), 10),
+        }
+        logging.info(f"{exchange.upper()} result : {ohlcv}")
+        return ohlcv
+        
 
     def huobi(self):
         exchange = "huobi"
@@ -117,23 +120,24 @@ class CryptoCandle:
         try:
             get_data = requests.get(f"{api_url}", params=params, timeout=5)
             get_data.raise_for_status()
-            logging.info(f"{exchange.upper()} API : {get_data.url}")
-            coin_data = get_data.json()
-            candle = coin_data["data"][1]
-            logging.info(f"{exchange.upper()} initial candle : {candle}")
-            ohlcv = {
-                "Timestamp": candle["id"],
-                "Open": round(float(candle["open"]), 1),
-                "Close": round(float(candle["close"]), 1),
-                "High": round(float(candle["high"]), 1),
-                "Low": round(float(candle["low"]), 1),
-                "Volume": round(float(candle["amount"]), 10),
-            }
-            logging.info(f"{exchange.upper()} result : {ohlcv}")
-            return ohlcv
-        except (requests.RequestException, ValueError) as err:
-            logging.info(f"{exchange.upper()} ERROR: ", err)
-            return False
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"{exchange.upper()} ERROR: {err}")
+            return False    
+        logging.info(f"{exchange.upper()} API : {get_data.url}")
+        coin_data = get_data.json()
+        candle = coin_data["data"][1]
+        logging.info(f"{exchange.upper()} initial candle : {candle}")
+        ohlcv = {
+            "Timestamp": candle["id"],
+            "Open": round(float(candle["open"]), 1),
+            "Close": round(float(candle["close"]), 1),
+            "High": round(float(candle["high"]), 1),
+            "Low": round(float(candle["low"]), 1),
+            "Volume": round(float(candle["amount"]), 10),
+        }
+        logging.info(f"{exchange.upper()} result : {ohlcv}")
+        return ohlcv
+        
 
     def kraken(self):
         exchange = "kraken"
@@ -147,36 +151,41 @@ class CryptoCandle:
         try:
             get_data = requests.get(api_url, params=params, timeout=5)
             get_data.raise_for_status()
-            logging.info(f"{exchange.upper()} API : {get_data.url}")
-            coin_data = get_data.json()
-            # Так как в запросе нельзя настроить глубину выдачи данных
-            # то запрос отдает несколько сотен свечей. Нам нужна для расчета
-            # только последняя. В этом json есть запись в конце вида
-            # "last":1576159200 - таймкод последней свечи. По нему и найдем.
-            last_minute = coin_data["result"]["last"]
-            logging.info(f"{exchange.upper()} last minute : {last_minute}",)
-            # В получаемом JSON у списка свечей ключ имеет завание 'XXBTZUSD'
-            # то есть имя валютной пары разбито на X_XBT_Z_USD
-            for candle in coin_data["result"][f"X{sym[:3]}Z{sym[3:]}"]:
-                if candle[0] == last_minute:
-                    logging.info(f"{exchange.upper()} initial candle : {candle}")
-                    ohlcv = {
-                        "Timestamp": candle[0],
-                        "Open": float(candle[1]),
-                        "Close": float(candle[4]),
-                        "High": float(candle[2]),
-                        "Low": float(candle[3]),
-                        "Volume": float(candle[6]),
-                    }
-                    logging.info(f"{exchange.upper()} result : {ohlcv}")
-                    return ohlcv
-
-        except (requests.RequestException, ValueError):
-            logging.info(f"{exchange.upper()} ERROR: ", err)
-            return False
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"{exchange.upper()} ERROR: {err}")
+            return False    
+        logging.info(f"{exchange.upper()} API : {get_data.url}")
+        coin_data = get_data.json()
+        # Так как в запросе нельзя настроить глубину выдачи данных
+        # то запрос отдает несколько сотен свечей. Нам нужна для расчета
+        # только последняя. В этом json есть запись в конце вида
+        # "last":1576159200 - таймкод последней свечи. По нему и найдем.
+        last_minute = coin_data["result"]["last"]
+        logging.info(f"{exchange.upper()} last minute : {last_minute}",)
+        # В получаемом JSON у списка свечей ключ имеет завание 'XXBTZUSD'
+        # то есть имя валютной пары разбито на X_XBT_Z_USD
+        for candle in coin_data["result"][f"X{sym[:3]}Z{sym[3:]}"]:
+            if candle[0] == last_minute:
+                logging.info(f"{exchange.upper()} initial candle : {candle}")
+                ohlcv = {
+                    "Timestamp": candle[0],
+                    "Open": float(candle[1]),
+                    "Close": float(candle[4]),
+                    "High": float(candle[2]),
+                    "Low": float(candle[3]),
+                    "Volume": float(candle[6]),
+                }
+                logging.info(f"{exchange.upper()} result : {ohlcv}")
+                return ohlcv
 
     def get_previous_candle_time(self):
-        get_utc_time = requests.get("https://yandex.com/time/sync.json").json()
+        try:
+            get_utc_time = requests.get("https://yandex.com/time/sync.json")
+            get_utc_time.raise_for_status()
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+            logging.error(f"GET TIME ERROR: {err}")
+            return False
+        get_utc_time = get_utc_time.json()
         # Забираем и форматируем Timestamp (отбрасываем милисекунды)
         utc_ts = int(str(get_utc_time["time"])[:-3])
         # Получаем текущую минуту
@@ -187,54 +196,62 @@ class CryptoCandle:
         )
         logging.info(f"Formed candle time: {ts}")
         return ts
+        
 
     def result_candle(self):
-        ohlcv_list = []
         candle_time = self.get_previous_candle_time()
-        # Получаем последнюю свечу с каждой биржи
-        bitfinex_candle = self.bitfinex()
-        binance_candle = self.binance()
-        hitbtc_candle = self.hitbtc()
-        huobi_candle = self.huobi()
-        kraken_candle = self.kraken()
+        if candle_time:
+            ohlcv_list = []
+            # Получаем последнюю свечу с каждой биржи
+            bitfinex_candle = self.bitfinex()
+            binance_candle = self.binance()
+            hitbtc_candle = self.hitbtc()
+            huobi_candle = self.huobi()
+            kraken_candle = self.kraken()
 
-        # Формируем список последних свечей
-        if bitfinex_candle and bitfinex_candle["Timestamp"] == candle_time:
-            ohlcv_list.append(bitfinex_candle)
-        if binance_candle and binance_candle["Timestamp"] == candle_time:
-            ohlcv_list.append(binance_candle)
-        if hitbtc_candle and hitbtc_candle["Timestamp"] == candle_time:
-            ohlcv_list.append(hitbtc_candle)
-        if huobi_candle and huobi_candle["Timestamp"] == candle_time:
-            ohlcv_list.append(huobi_candle)
-        if kraken_candle and kraken_candle["Timestamp"] == candle_time:
-            ohlcv_list.append(kraken_candle)
+            # Формируем список последних свечей
+            if bitfinex_candle and bitfinex_candle["Timestamp"] == candle_time:
+                ohlcv_list.append(bitfinex_candle)
+            if binance_candle and binance_candle["Timestamp"] == candle_time:
+                ohlcv_list.append(binance_candle)
+            if hitbtc_candle and hitbtc_candle["Timestamp"] == candle_time:
+                ohlcv_list.append(hitbtc_candle)
+            if huobi_candle and huobi_candle["Timestamp"] == candle_time:
+                ohlcv_list.append(huobi_candle)
+            if kraken_candle and kraken_candle["Timestamp"] == candle_time:
+                ohlcv_list.append(kraken_candle)
 
-        sum_vol = 0
-        sum_open = 0
-        sum_close = 0
-        sum_high = 0
-        sum_low = 0
-        for c in ohlcv_list:
-            sum_vol += c["Volume"]
-        for c in ohlcv_list:
-            sum_open += c["Open"] * c["Volume"] / sum_vol
-            sum_close += c["Close"] * c["Volume"] / sum_vol
-            sum_high += c["High"] * c["Volume"] / sum_vol
-            sum_low += c["Low"] * c["Volume"] / sum_vol
-        sum_ohlcv = {
-            "Timestamp": ohlcv_list[0]["Timestamp"],
-            "Open": round(sum_open, 1),
-            "Close": round(sum_close, 1),
-            "High": round(sum_high, 1),
-            "Low": round(sum_low, 1),
-            "Volume": round(sum_vol, 1),
-        }
-        logging.info(
-            f"Final {self.interval} candle for {self.symbol} equal : {sum_ohlcv}"
-        )
-
-        return sum_ohlcv
+            if len(ohlcv_list)>0:
+                sum_vol = 0
+                sum_open = 0
+                sum_close = 0
+                sum_high = 0
+                sum_low = 0
+                for c in ohlcv_list:
+                    sum_vol += c["Volume"]
+                for c in ohlcv_list:
+                    sum_open += c["Open"] * c["Volume"] / sum_vol
+                    sum_close += c["Close"] * c["Volume"] / sum_vol
+                    sum_high += c["High"] * c["Volume"] / sum_vol
+                    sum_low += c["Low"] * c["Volume"] / sum_vol
+                sum_ohlcv = {
+                    "Timestamp": candle_time,
+                    "Open": round(sum_open, 1),
+                    "Close": round(sum_close, 1),
+                    "High": round(sum_high, 1),
+                    "Low": round(sum_low, 1),
+                    "Volume": round(sum_vol, 1),
+                }
+                logging.info(
+                    f"Final {self.interval} candle for {self.symbol} equal : {sum_ohlcv}"
+                )
+                return sum_ohlcv
+            else:
+                logging.error("ohlcv_list is empty")
+                return False
+        else:
+            logging.error("time for the previous candle is not received")
+            return False
 
 
 candle = CryptoCandle("BTCUSD", "1m")
